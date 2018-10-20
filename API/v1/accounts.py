1f81626eaf621b4b336f4f20ee95dc4511323d9b
p@rick <frzpete@gmail.com>
2018-10-21 01:28:03 +0300

    Module structure organised for api design

--- API/v1/accounts.py
+++ API/v1/accounts.py
@@ -1,2 +1,99 @@
+"""
+This module deals with manipulation of accounts
+"""
 class Accounts:
+    admin={ 
+        1 : {
+            "username":"admin",
+            "password":"pass"
+        }
+    }
+    
+    attendants={
+        1 : {
+            'username' : 'mchwezi',
+            'password' : 'pass',
+            '' : ''
+        },
+        2 : {
+            'username' : 'mhombi',
+            'password' : 'pass'
+        },
+        3 : {
+            'username' : 'angy',
+            'pass' : 'pass'
+        },
+        4 : {
+            'username' : 'adele',
+            'password' : 'pass'
+        }
+    }
+
+    activeuser=[]
+
+    def Accounts(self):
+        pass
+
+
+    def loginAdmin(self, dict):
+        """
+        Verifies admin details to allow admin login
+        params: dictionary with key as username, value as password
+        return: id,True if verified else false
+        """
+        pass
+
+    
+    def login(self,dict):
+        """
+        Verifies Store Attendant login
+        params: dictionary with key as username, and value as the password entered by the user
+        return: id,True if verified else false
+        """
+        pass
+
+
+    def addNewAccount(self, dict):
+        """
+        Adds New Store Attendant account to inventory
+        params: dictionary containing key as username, value as password
+        return: true if added
+        """
+        pass
+    
+
+    def updateExistingAccount(self, dict):
+        """
+        Updates information of existing account
+        params: dictionary with key as id, value as dictionary containing updated information
+        return: true if added
+        """
+        pass
+
+
+    def deleteAccount(self, id):
+        """
+        Deletes selected user from the system
+        params: user id
+        return: true if deleted
+        """
+        pass
+
+
+    def deleteAllAccounts(self):
+        """
+        Deletes all Store Attendant Accounts in inventory
+        params: n/a
+        returns: true if all Store attendant accounts are deleteds
+        """
+        pass
+
+
+    def checkIfAccountExists(self, dict):
+        """
+        Checks the existing account details based on user input
+        params: dictionary containing with key as username, value as password
+        returns: id,true if exists else false
+        """
+        pass
     pass
--- API/v1/test_storemanager.py
+++ API/v1/test_storemanager.py
@@ -14,41 +14,83 @@ class TestStoreManager():
 		b = isinstance(self.records, Records)
 		c = isinstance(self.accounts, Accounts)
 
-
+#products
 	def test_addProduct(self):
 		"""
-		a
+		test addition of product
 		params: n/a
 		returns: n/a
 		"""
 		pass
+
 
 	def test_deleteProduct(self):
 		"""
-		a
+		Test deletion of a product
 		params: n/a
 		returns: n/a
 		"""
 		pass
 
+	def test_deleteAllProducts(self):
+		"""
+		Test deletion of all Products
+		params: n/a
+		returns: n/a
+		"""
+		pass
+
+
 	def test_updateExistingProduct(self):
 		"""
-		a
+		test update of product
 		params: n/a
 		returns: n/a
 		"""
 		pass
 	
+
 	def test_viewAllProducts(self):
 		"""
-		Test if all products can 
+		Test if all products can be viewed
 		params: n/a
 		returns: n/a
 		"""
 		pass
+	
+
+#accounts
+	def test_addAccount(self):
+		"""
+		Test that an account can be added
+		params: n/a
+		returns: n/a
+		"""
+		pass
+
+
+	def test_deleteAllAttendantAccounts(self):
+		"""
+		Test that all attendant accounts can be deleted
+		params: n/a
+		returns: n/a
+		"""
+		pass
+
+
+	def test_updateExistingAccount(self):
+		"""
+		test that an existing account can be updated according to user preferences
+		params: n/a
+		returns: n/a
+		"""
+		pass
+
+
 	@pytest.mark.skip(reason="no way of currently testing this")
 	def test_methodThatMayNotBeTestable(self):
 		pass
+
 
 	def tearDown(self):
 		"""
